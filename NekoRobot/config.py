c6ace08652352cbd1c35072a2fac5bc450c5a652
MOGAMBO <93328169+MOGAMBO-XD@users.noreply.github.com>
2023-06-23 19:59:11 +0530

    Add files via upload

---
+++ NekoRobot/config.py
@@ -0,0 +1,74 @@
+import json
+import os
+
+
+def get_user_list(config, key):
+    with open("{}/NekoRobot/{}".format(os.getcwd(), config), "r") as json_file:
+        return json.load(json_file)[key]
+
+
+class Config(object):
+    LOGGER = True
+
+    API_ID = "6447873"
+    API_HASH = "48b0315fcfa1759812fb9c651f5e75aa"
+    TOKEN = "5635289324:AAEJ1trXwDcTGxEnDtsD0HSW6YxubGfXnAg"
+    OWNER_ID = "5136853481"
+    OWNER_USERNAME = "Offline_into_100"
+    SUPPORT_CHAT = "CyberSupportGroup"
+    JOIN_LOGGER = ()
+    EVENT_LOGS = ()
+
+    SQLALCHEMY_DATABASE_URI = "postgres://cemldrbi:aFsK0urD6fWaFniJNcJRCwq3Z7110esC@jelani.db.elephantsql.com/cemldrbi"
+    MONGO_DB_URI = "mongodb+srv://bsdk:betichod@cluster0.fgj1r9z.mongodb.net/?retryWrites=true&w=majority"
+    LOAD = []
+    NO_LOAD = ["rss"]
+    WEBHOOK = False
+    INFOPIC = True
+    URL = None
+
+    # OPTIONAL
+    ##List of id's -  (not usernames) for users which have sudo access to the bot.
+    DRAGONS = get_user_list("elevated_users.json", "sudos")
+    ##List of id's - (not usernames) for developers who will have the same perms as the owner
+    DEV_USERS = get_user_list("elevated_users.json", "devs")
+    ##List of id's (not usernames) for users which are allowed to gban, but can also be banned.
+    DEMONS = get_user_list("elevated_users.json", "supports")
+    # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
+    TIGERS = get_user_list("elevated_users.json", "tigers")
+    WOLVES = get_user_list("elevated_users.json", "whitelists")
+
+    DONATION_LINK = ""
+    CERT_PATH = None
+    PORT = 5000
+    DEL_CMDS = True
+    STRICT_GBAN = True
+    WORKERS = (
+        8  # Number of subthreads to use. Set as number of threads your processor uses
+    )
+    BAN_STICKER = (
+        "CAACAgUAAxkBAAEDafNhq5Z0DegqVzauwSighMw5cPWp8QACVgQAAuUG0FRXfCEuBziNzCIE"
+    )
+    ALLOW_EXCL = True
+    CASH_API_KEY = ""
+    TIME_API_KEY = ""
+    BL_CHATS = []
+    SPAMMERS = None
+    ALLOW_CHATS = True
+    START_IMG = ""
+    HEROKU_API_KEY = None
+    HEROKU_APP_NAME = None
+    TEMP_DOWNLOAD_DIRECTORY = "./"
+   # ARQ_API_KEY = None
+   #ARQ_API_URL = None
+    ALLOW_EXCL = None
+    BL_CHATS = []  # List of groups that you want blacklisted.
+    SPAMMERS = None
+
+
+class Production(Config):
+    LOGGER = True
+
+
+class Development(Config):
+    LOGGER = True
