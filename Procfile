0a923e831194c4e6bc613d25731fe0ef6c12cefc
MOGAMBO <93328169+MOGAMBO-XD@users.noreply.github.com>
2023-06-23 19:55:18 +0530

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,2 @@
+worker: python3 -m NekoRobot
+ps:scale worker=1
---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+TITLE Neko Robot
+:: Enables virtual env mode and then starts Neko
+env\scripts\activate.bat && py -m NekoRobot
