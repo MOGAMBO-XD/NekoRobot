1d997a77a70cfed71dc434a4f8f6a4cc981246eb
MOGAMBO <93328169+MOGAMBO-XD@users.noreply.github.com>
2023-06-29 12:56:22 +0530

    Update __main__.py

--- NekoRobot/__main__.py
+++ NekoRobot/__main__.py
@@ -235,7 +235,7 @@ def start(update: Update, context: CallbackContext):
         else:
             first_name = update.effective_user.first_name
             update.effective_message.reply_sticker(
-                "CAACAgUAAxkBAAJYsmLWRvm70cE-mmxSNCovEf4v1ueJAAIcCAACbMK4VuL4EmZEkq8WKQQ"
+                "CAACAgUAAxkBAAED4i9knRVWtdtzTbSM7FhEjJTnNORcewACagsAAiMQyFStj95ITTbhjy8E"
             )
             update.effective_message.reply_text(
                 PM_START_TEXT.format(escape_markdown(first_name), BOT_NAME),
