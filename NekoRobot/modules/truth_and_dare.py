26f74cfd855f1e830fff5061fd3f7aa589a80235
MOGAMBO <93328169+MOGAMBO-XD@users.noreply.github.com>
2023-06-29 19:44:06 +0530

    Update truth_and_dare.py

--- NekoRobot/modules/truth_and_dare.py
+++ NekoRobot/modules/truth_and_dare.py
@@ -7,28 +7,27 @@ from NekoRobot.modules.disable import DisableAbleCommandHandler
 
 
 def truth(update: Update, context: CallbackContext):
-    context.args
-    truth = requests.get("https://elianaapi.herokuapp.com/games/truth").json()
-    truth = truth.get("truth")
+    truth = requests.get(f"https://api.truthordarebot.xyz/v1/truth").json()["question"]
     update.effective_message.reply_text(truth)
 
 
 def dare(update: Update, context: CallbackContext):
-    context.args
-    dare = requests.get("https://elianaapi.herokuapp.com/games/dares").json()
-    dare = dare.get("dare")
+    dare = requests.get(f"https://api.truthordarebot.xyz/v1/dare").json()["question"]
     update.effective_message.reply_text(dare)
 
 
-TRUTH_HANDLER = DisableAbleCommandHandler("truth", truth)
-DARE_HANDLER = DisableAbleCommandHandler("dare", dare)
+TRUTH_HANDLER = DisableAbleCommandHandler("truth", truth, run_async=True)
+DARE_HANDLER = DisableAbleCommandHandler("dare", dare, run_async=True)
 
 dispatcher.add_handler(TRUTH_HANDLER)
 dispatcher.add_handler(DARE_HANDLER)
 
+
 __help__ = """
 *Truth & Dare*
+
  ❍ /truth *:* Sends a random truth string.
  ❍ /dare *:* Sends a random dare string.
 """
+
 __mod_name__ = "Tʀᴜᴛʜ-Dᴀʀᴇ"
