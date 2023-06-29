40958d1cdc37f7e8b22a4d475b1a180ac21ae751
MOGAMBO <93328169+MOGAMBO-XD@users.noreply.github.com>
2023-06-29 18:13:20 +0530

    Update filters.py

--- NekoRobot/modules/helper_funcs/filters.py
+++ NekoRobot/modules/helper_funcs/filters.py
@@ -5,38 +5,44 @@ from NekoRobot import DEV_USERS, DRAGONS
 
 
 class CustomFilters(object):
-    class _Sudoers(BaseFilter):
+    class _Supporters(MessageFilter):
+        def filter(self, message: Message):
+            return bool(message.from_user and message.from_user.id in DEMONS)
+
+    support_filter = _Supporters()
+
+    class Sudoers(MessageFilter):
         def filter(self, message: Message):
             return bool(message.from_user and message.from_user.id in DRAGONS)
 
-    sudo_filter = _Sudoers()
+    sudo_filter = Sudoers()
 
-    class _Developers(BaseFilter):
+    class Developers(MessageFilter):
         def filter(self, message: Message):
             return bool(message.from_user and message.from_user.id in DEV_USERS)
 
-    dev_filter = _Developers()
+    dev_filter = Developers()
 
-    class _MimeType(BaseFilter):
+    class _MimeType(MessageFilter):
         def __init__(self, mimetype):
             self.mime_type = mimetype
             self.name = "CustomFilters.mime_type({})".format(self.mime_type)
 
         def filter(self, message: Message):
             return bool(
-                message.document and message.document.mime_type == self.mime_type
+                message.document and message.document.mime_type == self.mime_type,
             )
 
     mime_type = _MimeType
 
-    class _HasText(BaseFilter):
+    class _HasText(MessageFilter):
         def filter(self, message: Message):
             return bool(
                 message.text
                 or message.sticker
                 or message.photo
                 or message.document
-                or message.video
+                or message.video,
             )
 
     has_text = _HasText()
